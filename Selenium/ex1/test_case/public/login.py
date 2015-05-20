246e2a9928ad174dd5394dc549b6830114647278
Huang <Huang@Xinzuji-Test>
2015-05-20 17:15:37 +0800

    ok

--- Selenium/ex1/test_case/public/login.py
+++ Selenium/ex1/test_case/public/login.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import xml.dom.minidom as xdm
+dom = xdm.parse('..\\test_data\\login_data.xml')
+root = dom.documentElement
+
 def login(self,uname,psd):
     driver = self.driver
     driver.find_element_by_id("idInput").clear()
@@ -11,3 +15,16 @@ def login(self,uname,psd):
 def logout(self):
     driver = self.driver
     driver.find_element_by_link_text(u'退出').click()
+    
+def keyadd(self,key):
+    driver = self.driver
+    driver.get(self.base_url)
+    logins = root.getElementsByTagName(key)
+    uname = logins[0].getAttribute('uname')
+    psd = logins[0].getAttribute('psd')
+    prompt_info = logins[0].firstChild.data
+    
+    login(self, uname, psd)
+        
+    text = driver.find_element_by_xpath("//div[@class='error-tt']/p").text
+    self.assertEqual(text, prompt_info)
--- Selenium/ex1/test_case/test_login.py
+++ Selenium/ex1/test_case/test_login.py
@@ -8,23 +8,19 @@ Created on 2015年5月20日
 
 import unittest
 from selenium import webdriver
-import xml.dom.minidom as xdm
 from public import login
 
 
-dom = xdm.parse('..\\test_data\\login_data.xml')
-root = dom.documentElement
-
-class test_login(unittest.TestCase):
+class Test_Login(unittest.TestCase):
 
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.profileDir = 'D:\\FirefoxProfile'
+        self.profile = webdriver.FirefoxProfile(self.profileDir)
+        self.driver = webdriver.Firefox(self.profile)
         self.driver.implicitly_wait(30)
-        logins = root.getElementsByTagName('url')
-        self.base_url = logins[0].firstChild.data
+        self.base_url = 'http://mail.163.com'
         self.verificationErrors =[]
-
 
     def tearDown(self):
         self.driver.quit()
@@ -32,21 +28,16 @@ class test_login(unittest.TestCase):
 
 
     def test_null(self):
-        driver = self.driver
-        driver.get(self.base_url)
-        logins = root.getElementsByTagName('null')
-        uname = logins[0].getAttribute('uname')
-        psd = logins[0].getAttribute('psd')
-        prompt_info = logins[0].firstChild.data
-        
-        login.login(self,uname, psd)
-        text = driver.find_element_by_xpath("//div[@class='error-tt']/p").text
-        self.assertEqual(text, prompt_info)
+        login.keyadd(self, 'null')
         
     def test_null_psd(self):
-        driver = self.driver
-        driver.get(self.base_url)
-        logins = root.getElementsByTagName('null_psd')
+        login.keyadd(self, 'null_psd')
+        
+    def test_null_uname(self):
+        login.keyadd(self, 'null_uname')
+    
+    def test_err(self):
+        login.keyadd(self, 'err')
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
